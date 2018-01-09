567c8f26a55a8d5a132ad22163bcd950dcf5c50f
droid77daniel <enascudaniel77@gmail.com>
2018-01-09 20:29:40 +0000

    Initial commit

---
+++ app/src/main/java/com/example/marcoandrei/first_aplication/Main.java
@@ -0,0 +1,13 @@
+package com.example.marcoandrei.first_aplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
